a35db11bcaf17fe95c6d333afe649d5280093e31
FearMy0s <mirko.giannetta96@gmail.com>
2022-04-28 15:52:04 +0200

    fine

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 let KM = prompt ("Km da percorrere");
 let Age = prompt ("Età Passeggero");
 let Prezzo = KM * 0.21;
-let ScontoAnziani = Prezzo * 40 / 100;
-let ScontoBambini = Prezzo * 20 / 100;
-let PrezzoScontato1 = Prezzo - ScontoBambini;
-let PrezzoScontato2 = Prezzo - ScontoAnziani;
-if (Age <= 18){console.log(PrezzoScontato1);}
-else if(Age >= 65){console.log(PrezzoScontato2);}
+
+
+
+if (Age <= 18){
+    let ScontoBambini = Prezzo * 20 / 100;
+    let PrezzoScontato1 = Prezzo - ScontoBambini;
+    console.log(PrezzoScontato1 + "€");}
+else if(Age >= 65){
+    let ScontoAnziani = Prezzo * 40 / 100;
+    let PrezzoScontato2 = Prezzo - ScontoAnziani;
+    console.log(PrezzoScontato2 + "€");}
 else {
-    console.log(Prezzo)
-}
+    console.log(Prezzo + "€");}
