7afedfc3b7ba679dbcf9c3ca485ef931cd9a9dc5
FearMy0s <mirko.giannetta96@gmail.com>
2022-04-28 15:23:45 +0200

    updatre

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 let KM = prompt ("Km da percorrere");
 let Age = prompt ("Età Passeggero");
 let Prezzo = KM * 0.21;
-let ScontoBambini = Prezzo * 20;
-if (Age <= 18){
-    ScontoBambini};
+let ScontoBambini = Prezzo * 20 %100;
+if (Age <= 18){   ScontoBambini};
 console.log(Prezzo +"€");
