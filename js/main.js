721085e1ba491cb31e5bd4fc0cfbcafe9cde3933
FearMy0s <mirko.giannetta96@gmail.com>
2022-04-28 15:19:25 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let KM = prompt ("Km da percorrere");
+let Age = prompt ("Età Passeggero");
+let Prezzo = KM * 0.21;
+let ScontoBambini = Prezzo * 20;
+if (Age <= 18){
+    ScontoBambini};
+console.log(Prezzo +"€");
